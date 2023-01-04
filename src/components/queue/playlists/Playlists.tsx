3e9e3a927f58808b4a1ee9aae578241bbc2644fc
Jakob <j.wilmesmeier@gmail.com>
2023-01-04 19:32:58 +0100

    playlist adjustments

--- src/components/queue/playlists/Playlists.tsx
+++ src/components/queue/playlists/Playlists.tsx
@@ -26,15 +26,17 @@ export default function Playlists(props: PlaylistsProps) {
 			];
 			rep = reversedArray
 				.map((s) =>
-					s.length > 11
+					s.length > 14
 						? s.split("").reverse().join("")
-						: s.split("").reverse().join("").slice(0, 11)
+						: s.split("").reverse().join("").slice(0, 12)
 				)
 				.reverse();
 			range++;
-		} while (rep[0].length > 11);
+		} while (rep[0].length > 12);
 		console.log("output", rep);
-		return rep;
+		return rep.map((s) =>
+			s.length > 12 ? s.slice(0, 10).concat("...") : s
+		);
 	}
 
 	function openPlaylist(playlist: PlaylistDto) {
@@ -55,22 +57,22 @@ export default function Playlists(props: PlaylistsProps) {
 							className="flex w-0.45 h-full bg-bgPlaylist rounded-lg"
 						>
 							<img
-								className="w-1/3 h-full rounded-lg"
+								className="w-0.45 h-full rounded-lg"
 								src={playlist1.cover?.href}
 								alt="Cover"
 							></img>
 							<div className="flex flex-col w-2/3 h-full">
-								<p className="w-full h-3 mx-2 font-bold">
+								<p className="w-full h-3 mx-2 mt-0.5 font-bold text-sm">
 									{playlist1.name!.length > 11
 										? splitReverse(playlist1.name!)[0]
 										: playlist1.name}
 								</p>
 								{playlist1.name!.length > 11 && (
-									<p className="w-full h-3 mt-1 mx-2 font-bold">
+									<p className="w-full h-3 mt-1 mx-2 font-bold text-sm">
 										{splitReverse(playlist1.name!)[1]}
 									</p>
 								)}
-								<p className="w-full h-2 mt-2 ml-2">
+								<p className="w-full h-2 mt-3 ml-2 text-xs">
 									{"By " + playlist1.owner}
 								</p>
 							</div>
@@ -80,22 +82,22 @@ export default function Playlists(props: PlaylistsProps) {
 							className="flex w-0.45 h-full bg-bgPlaylist rounded-lg"
 						>
 							<img
-								className="w-1/3 h-full rounded-lg"
+								className="w-0.45 h-full rounded-lg"
 								src={playlist2.cover?.href}
 								alt="Cover"
 							></img>
 							<div className="flex flex-col w-2/3 h-full">
-								<p className="w-full h-3 mx-2 font-bold">
+								<p className="w-full h-3 mt-0.5 mx-2 font-bold text-sm">
 									{playlist2.name!.length > 11
 										? splitReverse(playlist2.name!)[0]
 										: playlist2.name}
 								</p>
 								{playlist2.name!.length > 11 && (
-									<p className="w-full h-3 mt-1 mx-2 font-bold">
+									<p className="w-full h-3 mt-1 mx-2 font-bold text-sm">
 										{splitReverse(playlist2.name!)[1]}
 									</p>
 								)}
-								<p className="w-full h-2 mt-2 ml-2">
+								<p className="w-full h-2 mt-3 ml-2 text-xs">
 									{"By " + playlist2.owner}
 								</p>
 							</div>
